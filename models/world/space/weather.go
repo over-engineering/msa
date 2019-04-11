2ae29899a70b2cc1728f728e7b2bf2dce49c3c57
Chan Park <cpark995@gmail.com>
2019-04-11 15:51:12 +0900

    Add weather

--- models/world/space/weather.go
+++ models/world/space/weather.go
@@ -1,1 +1,13 @@
 package space
+
+import "github.com/over-engineering/msa/models/types"
+
+type WeatherType types.Type
+
+const (
+	Sunny WeatherType = iota // 0
+	Rainy
+	Cloudy
+)
+
+// 습도, 미세먼지 농도 ...
