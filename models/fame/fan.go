60fbd8d6d1d1caad2519e4fb7a26bd224ba7fbf0
Chan Park <cpark995@gmail.com>
2019-04-12 11:18:33 +0900

    Revise the directory structure to have common

--- models/fame/fan.go
+++
@@ -1,9 +0,0 @@
-package fame
-
-// FanInfo shows the data about the character's influence.
-// We may consider 'set'-typed struct.
-// And also, we may use the fan info of the character's team.
-type FanInfo struct {
-	Fan  int `json:"fan"`
-	Anti int `json:"anti"`
-}
--- models/goods/interface.go
+++
@@ -1,9 +0,0 @@
-package goods
-
-// UniqueInfos represents static, unique information of goods.
-type UniqueInfos interface{}
-
-// OwnedGoods represents owned goods by owner.
-type OwnedGoods interface {
-	UpdateByDay() error
-}
